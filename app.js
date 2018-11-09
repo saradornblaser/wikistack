1c722a524035b0b8400bc96dbc4db0bcb2c9cefa
VandyWebDev <39500384+VandyWebDev@users.noreply.github.com>
2018-11-09 12:03:23 -0500

    initial setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan("dev "));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false}));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+const PORT = 1337;
+app.listen(PORT, () => {
+  console.log(`App listening at ${PORT}`);
+})
