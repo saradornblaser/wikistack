91c45f462b998a72a9aa4a4cec8a4941915b87e9
Sara Dornblaser <saradornblaser@gmail.com>
2018-11-09 12:53:56 -0500

    up to index.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const layout = require('./views/layout');
 
 const app = express();
 app.use(morgan("dev "));
 app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  //res.send('Hello World!');
+  res.send(layout(''));
+
 })
 
-const PORT = 1337;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App listening at ${PORT}`);
 })
